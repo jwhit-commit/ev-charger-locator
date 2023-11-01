2f8b1ceb87857b9d7f738a14eca9802405f26c99
cpapanicholas <cpapanicholas@gmail.com>
2023-11-01 11:13:27 -0500

    put code into handlebars and routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,43 +5,44 @@ const withAuth = require('../utils/auth');
 router.get('/', async (req, res) => {
     try {
       // Get all projects and JOIN with user data
-      const projectData = await Project.findAll({
-        include: [
-          {
-            model: User,
-            attributes: ['name'],
-          },
-        ],
-      });
+      // const projectData = await Project.findAll({
+      //   include: [
+      //     {
+      //       model: User,
+      //       attributes: ['name'],
+      //     },
+      //   ],
+      // });
   
       // Serialize data so the template can read it
-      const projects = projectData.map((project) => project.get({ plain: true }));
+      // const projects = projectData.map((project) => project.get({ plain: true }));
   
       // Pass serialized data and session flag into template
-      res.render('homepage', { 
-        projects, 
+      res.render('home', { 
+        // projects, 
         logged_in: req.session.logged_in 
       });
     } catch (err) {
+      console.error(err);
       res.status(500).json(err);
     }
   });
   
-  router.get('/project/:id', async (req, res) => {
+  router.get('//:id', async (req, res) => {
     try {
-      const projectData = await Project.findByPk(req.params.id, {
-        include: [
-          {
-            model: User,
-            attributes: ['name'],
-          },
-        ],
-      });
+      // const projectData = await Project.findByPk(req.params.id, {
+      //   include: [
+      //     {
+      //       model: User,
+      //       attributes: ['name'],
+      //     },
+      //   ],
+      // });
   
-      const project = projectData.get({ plain: true });
+      // const project = projectData.get({ plain: true });
   
-      res.render('project', {
-        ...project,
+      res.render('', {
+        // ...project,
         logged_in: req.session.logged_in
       });
     } catch (err) {
@@ -50,7 +51,7 @@ router.get('/', async (req, res) => {
   });
   
   // Use withAuth middleware to prevent access to route
-  router.get('/profile', withAuth, async (req, res) => {
+  router.get('/User', withAuth, async (req, res) => {
     try {
       // Find the logged in user based on the session ID
       const userData = await User.findByPk(req.session.user_id, {
--- models/index.js
+++ models/index.js
@@ -2,10 +2,10 @@ const User = require('./User');
 
 // const Project = require('./Project');
 
-User.hasMany(Project, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE'
-});
+// User.hasMany(Project, {
+//   foreignKey: 'user_id',
+//   onDelete: 'CASCADE'
+// });
 
 //Project.belongsTo(User, {
 //  foreignKey: 'user_id'
