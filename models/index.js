493c1d156097e596fda2983397f0b1eb5e15a75f
jwhit <28734055+jwhit-commit@users.noreply.github.com>
2023-11-01 14:24:23 -0500

    fixed models, login route and login function

--- models/index.js
+++ models/index.js
@@ -1,30 +1,4 @@
 const User = require('./User');
-
-const UserLocation = require('./Locations');
-// const Station = require('./Stations');
-
-
-// User.hasMany(Project, {
-//   foreignKey: 'user_id',
-//   onDelete: 'CASCADE'
-// });
-
-UserLocation.belongsTo(User, {
-  foreignKey: 'user_id',
-});
-
-// UserLocation.hasMany(Station,{
-//   foreignKey: 'location_id',
-//   onDelete: 'CASCADE'
-// });
-
-// Station.belongsTo(UserLocation,{
-//   foreignKey:'location_id',
-// })
-
-
-module.exports = { User, UserLocation};
-=======
 const Location = require('./Location');
 const UserLocation = require('./UserLocation');
 
