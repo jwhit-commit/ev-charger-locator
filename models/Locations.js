839b5fffb4cffeb8db2807d5feaf5f03c463f2d2
tanfei0704 <tanfei0704@gmail.com>
2023-11-01 11:40:30 -0400

    save

---
+++ models/Locations.js
@@ -0,0 +1,40 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class UserLocation extends Model { }
+
+UserLocation.init(
+    {
+    location_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    address: {
+        type: DataTypes.GEOGRAPHY('POINT'),
+        allowNull: false,
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'User',
+          key: 'id',
+        },
+    },
+    station_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'Station',
+          key: 'id',
+        },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'UserLocation'
+})
+
+
+
+module.exports = UserLocation;
---
+++ models/Stations.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Station extends Model { }
+
+Station. init(
+    {
+        id:{
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name:{
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        distance: {
+            type: DataTypes.DOUBLE,
+            allowNull: false,
+        },
+        address: {
+            type: DataTypes.STRING,
+            allowNull: false,
+          },
+        location_id:{
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'Location',
+                key: 'id',
+            },
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+              model: 'User',
+              key: 'id',
+            },
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'Station'
+});
+
+module.exports = Station;
